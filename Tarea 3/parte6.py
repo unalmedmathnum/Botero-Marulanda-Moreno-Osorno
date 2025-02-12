be7539a883bf3274b2f57e3b100c03ba56d55d29
JuanOsorner <juanpowerlifting100kg@gmail.com>
2025-02-12 16:15:54 -0500

    commit4

--- Tarea 3/parte6.py
+++ Tarea 3/parte6.py
@@ -3,3 +3,6 @@ import math
 def f(t,u,v):
     # Aquí u' = v
     return v
+def g(t,u,v,epsilon,lam,omega):
+    # Aqui tenemos u'' = v' 
+    return -(1+epsilon*lam*omega**2*math.cos(omega*t))*(u-(epsilon**2*u**3)/6.0)
